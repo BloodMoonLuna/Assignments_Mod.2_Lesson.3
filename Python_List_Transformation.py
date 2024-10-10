23e102f1660b30ed4e70e4f09eafb6c9742205bb
BloodMoonLuna <mmichiaels98@gmail.com>
2024-10-10 12:07:11 -0400

    first commit

---
+++ Python_List_Transformation.py
@@ -0,0 +1,9 @@
+#Objective: The aim of this assignment is to practice list operations and transformations. Problem Statement: You've been given a list of grades from an exam. You need to process and analyze these grades.
+#Task 1: Sort the grades in descending order and print the sorted list. Task 2: Calculate the average grade and print it.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+print(grades)
+
+average = (85+90+78+88+76+95+89+80+72+93)/10
+print(average)
